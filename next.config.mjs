7e4a2ca26dc6057d6b52c13a3d72d6ee6e5dbaee
FajarKrisdiantoro <fajarkrisdiantoro021205@gmail.com>
2024-12-30 14:07:56 +0700

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+import withPWA from "@ducanh2912/next-pwa";
+
+const nextConfig = {
+  reactStrictMode: true, // Aktifkan React Strict Mode (opsional)
+  pwa: {
+    dest: "public", // Menyimpan service worker di folder public
+    cacheOnFrontEndNav: true, // Mengaktifkan cache untuk navigasi frontend
+    aggressiveFrontEndNavCaching: true, // Cache agresif untuk navigasi
+    reloadOnOnline: true, // Memuat ulang ketika online
+    swcMinify: true, // Mengaktifkan SWC minifier
+    workboxOptions: {
+      disableDevLogs: true, // Menonaktifkan log dev Workbox
+    },
+  },
+};
+
+export default withPWA(nextConfig);
